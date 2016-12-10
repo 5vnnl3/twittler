2a0436fe874862a5c9e852b62b06f4c168084ef5
Sunnie Mao <mao.sunnie@gmail.com>
2016-12-09 17:57:05 -0800

    test creating branches

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -1,5 +1,5 @@
 /**
-Test manual comments (no -m)
+Test branches
  * Timeago is a jQuery plugin that makes it easy to support automatically
  * updating fuzzy timestamps (e.g. "4 minutes ago" or "about 1 day ago").
  *
